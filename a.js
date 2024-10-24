c82e0f7727b96408f9e70e156becadda42a54fbb
mmaurino18 <matiagus0312@gmail.com>
2024-10-24 20:46:46 -0300

    primer commit

--- a.js
+++ a.js
@@ -14,18 +14,19 @@ const alpaca = new Alpaca({
     secretKey: process.env.ALPACA_API_SECRET_KEY,
     paper: true,
 });
-const chat_id = process.env.TELEGRAM_CHAT_ID;
+
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 const generativeModel = genAI.getGenerativeModel({ model: "gemini-pro" });
+
 
 function startWebSocket() {
     const wss = new WebSocket("wss://stream.data.alpaca.markets/v1beta1/news");
 
     wss.on('open', function() {
         console.log("Websocket connected!");
-        sendMessageToTelegram('Me conecte correctamente', chat_id);
+        //sendMessageToTelegram('Me conecte correctamente', chat_id);
         //authenticate();
         const authMsg = {
             action: 'auth',
@@ -49,19 +50,15 @@ function startWebSocket() {
             if (currentEvent.T === "n") {
                 
                 
-             
+                //const companyImpactGPT = connectToGPT(currentEvent.headline);
+                const companyImpactGemini = connectToGemini(currentEvent.headline);
+                console.log(companyImpactGemini);
                 const symbol = currentEvent.symbols[0];
-                console.log(symbol);
-                const token_iol= await authenticate();
-                const flag = await symbolIsOk(symbol,token_iol);
-                if(flag){
-                    const companyImpactGemini = await connectToGemini(currentEvent.headline);
-                    console.log(companyImpactGemini);
-                    const companyImpactGPT = await connectToGPT(currentEvent.headline);
-                    console.log(companyImpactGPT);
-                    console.log(`Compra esta accionnnn ${symbol}`)
-                }
-                
+
+                //if(companyImpactGemini >= 70){
+                const token_iol= authenticate();
+                const price = getCotizacion(token_iol,symbol);
+                console.log(price);
                // }
 
             }
@@ -153,10 +150,9 @@ async function authenticate() {
 
 
 async function getCotizacion(token,symbol) {
-    const response = await fetch(`https://api.invertironline.com/api/v2/Cotizaciones/MEP/${symbol}`, {
+    const response = await fetch(`https://api.invertironline.com/api/v2/Cotizaciones/BCBA/${symbol}`, {
         method: 'GET',
         headers: {
-            //'Host' : 'api.invertironline.com',
             'Authorization': `Bearer ${token}`,
             'Content-Type': 'application/json'
         }
@@ -164,88 +160,14 @@ async function getCotizacion(token,symbol) {
 
     if (!response.ok) {
         const errorDetails = await response.text();
-        console.error(`Error al obtener cotización para ${symbol}: ${response.status} - ${errorDetails}`);
         throw new Error(`Error al obtener cotización: ${response.status} - ${errorDetails}`);
     }
 
     const data = await response.json();
     return data.ultimoPrecio;
 }
-async function symbolIsOk(symbol,token) {  // Asegúrate de tener el token actualizado
-    const apiURL = `https://api.invertironline.com/api/v2/bCBA/Titulos/${symbol}`;
-    console.log(apiURL);
-    const response = await fetch(apiURL, {
-        method: 'GET',
-        headers: {
-            'Authorization': `Bearer ${token}`,
-            'Content-Type': 'application/json'
-        }
-    });
-    console.log(response);
-    if (response.ok) {
-        const data = await response.json();
-        console.log(`El símbolo ${symbol} está disponible en IOL:`, data);
-        return true;
-    } else {
-        console.error(`El símbolo ${symbol} no está disponible en IOL. Error: ${response.status}`);
-        return false;
-    }
-}
-async function getSymbols(token) {
-    const url = 'https://api.invertironline.com/api/v2/bCBA/Titulos'; // Cambia bCBA si es necesario
-
-    try {
-        const response = await fetch(url, {
-            method: 'GET',
-            headers: {
-                'Authorization': `Bearer ${token}`,
-                'Content-Type': 'application/json'
-            }
-        });
-
-        // Verificar si la respuesta es exitosa
-        if (!response.ok) {
-            // Imprime más detalles del error
-            const errorText = await response.text(); // Para obtener más información del error
-            throw new Error(`HTTP error! Status: ${response.status}, Response: ${errorText}`);
-        }
-
-        const data = await response.json();
-        console.log(data);
-        return data;
-    } catch (error) {
-        console.error('Error al obtener los títulos:', error);
-    }
-}
-
-app.listen(port, async () => {
+app.listen(port, () => {
     console.log(`Servidor escuchando en el puerto ${port}`);
-    /*const token = await authenticate();
-    const symbols = await getSymbols(token);
-    console.log(symbols);*/
     startWebSocket();
     //startKeepAlive();
 });
-async function sendMessageToTelegram(message, chat_id) {
-    const botToken = process.env.TELEGRAM_BOT_TOKEN;
-    const apiUrl = `https://api.telegram.org/bot${botToken}/sendMessage`;
-    const requestBody = {
-        chat_id: chat_id,
-        text: message
-    };
-
-    try {
-        const response = await fetch(apiUrl, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(requestBody)
-        });
-
-        const responseData = await response.json();
-        //console.log('Message sent successfully:', responseData);
-    } catch (error) {
-        console.error('Failed to send message:', error);
-    }
-}
